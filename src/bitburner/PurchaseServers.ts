0b6e6837f6ebd0652a73328f1b421318a6eea67f
Dan Leinbach <leinb1dr@gmail.com>
2022-12-24 09:48:55 -0500

    Added upgrading of purchased servers so I can start smaller.

--- src/bitburner/PurchaseServers.ts
+++ src/bitburner/PurchaseServers.ts
@@ -5,26 +5,34 @@ function myMoney(ns: NS) {
 }
 
 async function waitForMoney(ns: NS, cost: number) {
-    while (myMoney(ns) < cost) {
-        ns.print(`Need $${cost}. Have $${myMoney(ns)}`);
+    while (myMoney(ns) < cost)
         await ns.sleep(1000);
-    }
 }
 
 /** @param {NS} ns */
 export async function main(ns: NS) {
-    
-    while(ns.getPurchasedServers().length < 25){
+    ns.disableLog("sleep");
+    ns.disableLog("getServerMoneyAvailable");
+
+    while (ns.getPurchasedServers().length < 25) {
         const cost = ns.getPurchasedServerCost(2);
         await waitForMoney(ns, cost);
         ns.purchaseServer("home", 2);
     }
 
-    for(let i = 2; 1<=20; i++){
-        for(let j = 0; j < ns.getPurchasedServers().length; j++){
-            const cost = ns.getPurchasedServerUpgradeCost(`home-${j}`, Math.pow(2, i));
-            await waitForMoney(ns, cost);
-            ns.upgradePurchasedServer(`home-${0}`, Math.pow(2, i));
+    for (let i = 2; 1 <= 20; i++) {
+        const ramUpgrade = Math.pow(2, i);
+        for (let j = 0; j < ns.getPurchasedServers().length; j++) {
+            if (ns.getServerMaxRam(`home-${j}`) < ramUpgrade) {
+                const cost = ns.getPurchasedServerUpgradeCost(`home-${j}`, ramUpgrade);
+                ns.print(`Waiting to purchase ${ramUpgrade} ram for home-${j} costing ${cost}`);
+                await waitForMoney(ns, cost);
+                const success = ns.upgradePurchasedServer(`home-${j}`, ramUpgrade);
+                ns.print(`Purchasing ${ramUpgrade} ram for home-${j} was successful: ${success}`);
+                if(success){
+                    ns.killall(`home-${j}`);
+                }
+            }
         }
     }
 
