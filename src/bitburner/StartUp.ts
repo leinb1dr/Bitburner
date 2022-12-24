e905b2258df6e804b9312f3c3a6700ff27b5a371
Dan Leinbach <leinb1dr@gmail.com>
2022-12-24 09:49:49 -0500

    Added hacking to startup script

--- src/bitburner/StartUp.ts
+++ src/bitburner/StartUp.ts
@@ -7,4 +7,9 @@ export async function main(ns: NS) {
     ns.run("HackingManager.js");
     ns.run("PurchaseHackNet.js", 1, 30);
     ns.run("PurchaseServers.js");
+
+    const totalRam = ns.getServerMaxRam("home")-ns.getServerUsedRam("home")+ns.getScriptRam("StartUp.js")-ns.getScriptRam("ConfigUpdater.js");
+    const hackRam = ns.getScriptRam("Hack.js");
+    const t = Math.round(totalRam / hackRam);
+    ns.spawn("Hack.js", t, t, 0);
 }
